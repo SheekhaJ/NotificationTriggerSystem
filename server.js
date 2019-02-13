b2db18b79d059dc52086dbdaf96857385dd3a381
SheekhaJ <jariwals@oregonstate.edu>
2019-02-12 22:42:27 -0800

    Moved js and css files to public and modified server.js to use ejs engine

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+
+var app = express();
+var port = 8080;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    // console.log("Hello world!");
+    res.render('index');
+});
+
+app.listen(port, function(){
+    console.log("app is running on port "+port);
+})
