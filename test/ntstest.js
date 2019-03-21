8e0b2ec19f01b03918c96567d2e47c804db67c73
SheekhaJ <jariwals@oregonstate.edu>
2019-03-21 11:00:08 -0700

    just a small test

--- test/ntstest.js
+++ test/ntstest.js
@@ -3,9 +3,8 @@ const expect = chai.expect;
 var test = require('../server.js');
 
 describe("sanity test", function(){
-    it("this test should run", function(error, done){
+    it("this test should run", function(error){
         expect(1+1).to.equal(2);
         expect(1+3).not.to.equal(5);
-        done();
     })    
 });
